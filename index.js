08c03efb77ef3e01416db0fdeff6966366541eaf
Asraf Ali <asraf.cse@gmail.com>
2022-10-11 19:11:01 +0530

    api from stock-nse-india

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const ex = require("express");
 const cors = require('cors')
 const { FmarketStatus, Fglossary , FmarketTurnover ,FequityMaster , Fholiday , FmergedDailyReports , FallIndices , FindexNames , Fequity ,FtradeInfo , FcorporateInfo , Fintraday ,Fhistorical ,Findex ,FindexIntaday} = require('./functions')
 
+const { NseIndia } = require('stock-nse-india')
+
+const nse = new NseIndia()
+
 //EXPRESS APP
 const app = ex() ; app.use(cors()) ;const port = process.env.PORT || 5000;
 // routes
@@ -22,4 +26,8 @@ app.get(`/historical/:params`, async (req, res) => {try {const param = req.param
 app.get(`/index/:params`, async (req, res) => {try {const param = req.params.params ;const api = await Findex(param);return res.status(200).send(api)}catch (err) {return res.status(500).json({err: err.toString()})}})
 app.get(`/indexIntaday/:params`, async (req, res) => {try {const param = req.params.params ;const api = await FindexIntaday(param);return res.status(200).send(api)}catch (err) {return res.status(500).json({err: err.toString()})}})
 
+app.get('/test', async (req,res)=>{
+    const result= await nse.getDataByEndpoint('/api/market-turnover')
+    res.status(200).send(result)
+})
 app.listen(port, () => {console.log(`running on port http://localhost:5000`)})
