49cbf4c42d0f78d86e199ccdc9b222f56c5a3133
aftabmk <kizhisery@gmail.com>
2022-10-18 18:53:00 +0530

    modified:   function/getCookie.js 	modified:   function/setCookie.js

--- function/setCookie.js
+++ function/setCookie.js
@@ -6,7 +6,6 @@ function setCookie()
     const join = "cookies="+hex +"O"+ hex + "O" +hex+"O" +hex + "O"+hex +hex + hex + hex + hex +'1010' + new Date()
     return join  
 }
-// console.log(setCookie())
 module.exports = { setCookie }
 
 
