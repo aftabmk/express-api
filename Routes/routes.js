856877dd9d8ad3ab05a9b6cac4fe7e619e7ec4b4
Aftab mohammed k <87110394+aftabmk@users.noreply.github.com>
2022-10-26 15:06:22 +0530

    Update routes.js

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,5 +1,5 @@
 const ex = require('express')
-const { FmarketStatus, Fautocomplete, FmarketTurnover, Fholiday, FallIndices, Fequity, FtradeInfo, FcorporateInfo, Foptions, FfinancialInfo, Fintraday, FindexPreOpenApi, Fhistorical, Findex, FindexIntaday, Findexhistorical, FinsiderApi, FlistingToday, FprevListing, FblockDeals } = require('../other/functions')
+const { FmarketStatus, Fautocomplete, FmarketTurnover, Fholiday, FallIndices, Fequity, FtradeInfo, FcorporateInfo, Foptions, FfinancialInfo, Fintraday, FindexPreOpenApi, Fhistorical, Findex, FindexIntaday, Findexhistorical, FinsiderApi, FlistingToday, FprevListing ,FtopGainer ,FtopLooser , FactiveStocks , FpriceBand , FvolumeGainers , FallTimeHigh , FallTimeLow , FlargeDeal , FshareHolding ,FPromoter , FPublic} = require('../other/functions')
 
 const router = ex.Router()
 
@@ -20,9 +20,18 @@ router.get(`/indexhistorical/:params`, async (req, res) => { try { const param =
 router.get(`/insider/:params`, async (req, res) => { try { const param = req.params.params; const api = await FinsiderApi(param); return res.status(200).send(api) } catch (err) { return res.status(500).json({ err: err.toString() }) } })
 router.get(`/prevListing`, async (req, res) => { try { const api = await FprevListing(); return res.status(200).send(api) } catch (err) { return res.status(500).json({ err: err.toString() }) } })
 router.get(`/listingToday`, async (req, res) => { try { const api = await FlistingToday(); return res.status(200).send(api) } catch (err) { return res.status(500).json({ err: err.toString() }) } })
-router.get(`/blockDeals`, async (req, res) => { try { const api = await FblockDeals(); return res.status(200).send(api) } catch (err) { return res.status(500).json({ err: err.toString() }) } })
 router.get(`/search/:params`, async (req, res) => { try { const param = req.params.params; const api = await Fautocomplete(param); return res.status(200).send(api) } catch (err) { return res.status(500).json({ err: err.toString() }) } })
 router.get(`/options/:params`, async (req, res) => { try { const param = req.params.params; const api = await Foptions(param); return res.status(200).send(api) } catch (err) { return res.status(500).json({ err: err.toString() }) } })
+router.get(`/topgainer` ,async (req, res) => { try { const api = await FtopGainer(); return res.send(api); } catch (err) { return res.status(500).json({ err: err.toString() }) } })
+router.get(`/toplooser` ,async (req, res) => { try { const api = await FtopLooser(); return res.send(api); } catch (err) { return res.status(500).json({ err: err.toString() }) } })
+router.get(`/activeStocks` ,async (req, res) => { try { const api = await FactiveStocks(); return res.send(api); } catch (err) { return res.status(500).json({ err: err.toString() }) } })
+router.get(`/priceBand` ,async (req, res) => { try { const api = await FpriceBand(); return res.send(api); } catch (err) { return res.status(500).json({ err: err.toString() }) } })
+router.get(`/volumeGainer` ,async (req, res) => { try { const api = await FvolumeGainers(); return res.send(api); } catch (err) { return res.status(500).json({ err: err.toString() }) } })
+router.get(`/alltimehigh` ,async (req, res) => { try { const api = await FallTimeHigh(); return res.send(api); } catch (err) { return res.status(500).json({ err: err.toString() }) } })
+router.get(`/alltimelow` ,async (req, res) => { try { const api = await FallTimeLow(); return res.send(api); } catch (err) { return res.status(500).json({ err: err.toString() }) } })
+router.get(`/largedeal` ,async (req, res) => { try { const api = await FlargeDeal(); return res.send(api); } catch (err) { return res.status(500).json({ err: err.toString() }) } })
+router.get(`/shareHolding/:params/promoter`, async (req, res) => { try { const param = req.params.params; const api = await FshareHolding(param); const promoter = await FPromoter(api) ;return res.status(200).send(promoter) } catch (err) { return res.status(500).json({ err: err.toString() }) } })
+router.get(`/shareHolding/:params/public`, async (req, res) => { try { const param = req.params.params; const api = await FshareHolding(param); const public = await FPublic(api) ;return res.status(200).send(public) } catch (err) { return res.status(500).json({ err: err.toString() }) } })
 
 
 module.exports = router
